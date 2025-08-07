68c093a06d18a009495b8ea72d3a3912289b4be1
Radomila <hejhalova.radomila@gmail.com>
2025-08-07 15:07:30 +0200

    fix: imports

--- src/app/(blog)/create/page.tsx
+++ src/app/(blog)/create/page.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import CreateEditPostForm from '@/components/forms/CreatePostForm/CreateEditPostForm';
 import ProtectedRoute from '@/components/core/ProtectedRoute';
 import BlogLayout from '@/components/layout/BlogLayout';
+import CreateEditPostForm from '@/components/forms/CreatePostForm/CreateEditPostForm';
 
 const CreatePost = () => {
   return (
